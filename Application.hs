51b9dc5c96d393b2a6b2407ec20523401da13ab8
Matthew Maurer <maurer@matthewmaurer.org>
2014-11-14 06:07:17 -0500

    Mockup can now set schedules

--- Application.hs
+++ Application.hs
@@ -31,6 +31,7 @@ import Handler.Distill
 import Handler.Task
 import Handler.Strike
 import Handler.Scheme
+import Handler.Schedule
 
 -- This line actually creates our YesodDispatch instance. It is the second half
 -- of the call to mkYesodData which occurs in Foundation.hs. Please see the
