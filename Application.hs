eeb3ca1d73b75a99513390d8f961a7581866ced2
Matthew Maurer <maurer@matthewmaurer.org>
2014-11-14 01:24:02 -0500

    Add initial strike route

--- Application.hs
+++ Application.hs
@@ -29,6 +29,7 @@ import Handler.Home
 import Handler.Jot
 import Handler.Distill
 import Handler.Task
+import Handler.Strike
 
 -- This line actually creates our YesodDispatch instance. It is the second half
 -- of the call to mkYesodData which occurs in Foundation.hs. Please see the
---
+++ Handler/Strike.hs
@@ -0,0 +1,6 @@
+module Handler.Strike where
+
+import Import
+
+getStrikeR :: Handler Html
+getStrikeR = error "Not yet implemented: getStrikeR"
