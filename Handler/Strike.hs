5d3af8fbd1ce3626e88f18e2396c13f8e45e34ff
Matthew Maurer <maurer@matthewmaurer.org>
2014-11-16 21:40:19 -0500

    Basic strike display

--- Handler/Strike.hs
+++ Handler/Strike.hs
@@ -1,6 +1,26 @@
 module Handler.Strike where
 
 import Import
+import Database.Persist.Sql
+
+taskPanel task = $(widgetFile "taskPanel")
 
 getStrikeR :: Handler Html
-getStrikeR = error "Not yet implemented: getStrikeR"
+getStrikeR = do
+  tasks <- runDB $ do
+    liveTaskIds <- selectKeysList [TaskCompleted ==.Nothing] []
+    schedIds <- fmap (map $ scheduleTask . entityVal) $
+                     selectList [ScheduleBonus ==. False
+                                ,ScheduleTask <-. liveTaskIds]
+                                [Asc SchedulePrio]
+    ids <-
+      if null schedIds
+        then fmap (map $ scheduleTask . entityVal) $
+                  selectList [ScheduleBonus ==. True
+                             ,ScheduleTask <-. liveTaskIds]
+                             [Asc SchedulePrio]
+        else return $ schedIds
+    selectList [TaskId <-. ids] [Asc TaskCreated]
+  defaultLayout $ do
+    setTitle "Strike."
+    $(widgetFile "strike")
