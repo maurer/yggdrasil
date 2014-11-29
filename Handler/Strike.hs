9d2ee9a4cd96715f45e5bcced73907eeb959e02e
Matthew Maurer <maurer@matthewmaurer.org>
2014-11-29 04:38:20 -0500

    Stripped out old scheme

--- Handler/Strike.hs
+++ Handler/Strike.hs
@@ -7,29 +7,7 @@ import Data.Text (pack)
 taskPanel task = $(widgetFile "taskPanel")
 
 getStrikeR :: Handler Html
-getStrikeR = do
-  tasks <- runDB $ do
-    liveTaskIds <- selectKeysList [TaskCompleted ==.Nothing] []
-    schedIds <- fmap (map $ scheduleTask . entityVal) $
-                     selectList [ScheduleBonus ==. False
-                                ,ScheduleTask <-. liveTaskIds]
-                                [Asc SchedulePrio]
-    ids <-
-      if null schedIds
-        then fmap (map $ scheduleTask . entityVal) $
-                  selectList [ScheduleBonus ==. True
-                             ,ScheduleTask <-. liveTaskIds]
-                             [Asc SchedulePrio]
-        else return $ schedIds
-    selectList [TaskId <-. ids] [Asc TaskCreated]
-  defaultLayout $ do
-    setTitle "Strike."
-    $(widgetFile "strike")
+getStrikeR = undefined
 
 getStrikeTaskR :: TaskId -> Handler Html
-getStrikeTaskR taskId = do
-  task <- runDB $ get404 taskId
-  let taskName = taskTitle task
-  defaultLayout $ do
-    setTitle $ "Strike: " <> (toHtml $ taskName)
-    $(widgetFile "strikeTask")
+getStrikeTaskR taskId = undefined
