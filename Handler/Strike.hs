b465ce55f8b34896bb382dcf9a23ce61f1d9c005
Matthew Maurer <maurer@matthewmaurer.org>
2014-11-29 05:11:32 -0500

    Restored basic strike functionality

--- Handler/Strike.hs
+++ Handler/Strike.hs
@@ -7,7 +7,15 @@ import Data.Text (pack)
 taskPanel task = $(widgetFile "taskPanel")
 
 getStrikeR :: Handler Html
-getStrikeR = undefined
+getStrikeR = do
+  tasks <- runDB $ selectList [TaskCompleted ==. Nothing] [Asc TaskCreated]
+  defaultLayout $ do
+    setTitle "Strike."
+    $(widgetFile "strike")
 
 getStrikeTaskR :: TaskId -> Handler Html
-getStrikeTaskR taskId = undefined
+getStrikeTaskR taskId = do
+  task <- runDB $ get404 taskId
+  defaultLayout $ do
+    setTitle "Strike."
+    $(widgetFile "strikeTask")
