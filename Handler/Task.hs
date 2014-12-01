d9c8c5892dc9e00a5115689d578aeeb4f23a6431
Matthew Maurer <maurer@matthewmaurer.org>
2014-11-30 21:10:29 -0500

    Added the ability to make a task with a parent

--- Handler/Task.hs
+++ Handler/Task.hs
@@ -2,21 +2,32 @@ module Handler.Task where
 
 import Import
 import Data.Time
+import Database.Persist.Sql
 
 postTaskR :: Handler TypedContent
 postTaskR = do
-  goal  <- runInputPost $ ireq textField "goal"
-  title <- runInputPost $ ireq textField "title"
-  exec  <- runInputPost $ ireq boolField "exec"
+  goal    <- runInputPost $ ireq textField "goal"
+  title   <- runInputPost $ ireq textField "title"
+  exec    <- runInputPost $ ireq boolField "exec"
+  mParent <- fmap (fmap toSqlKey) $ runInputPost $ iopt intField "parent"
   t    <- liftIO getCurrentTime
-  taskId <- runDB $ insert $ Task
-    { taskTitle = title
-    , taskCompletion = goal
-    , taskCreated = t
-    , taskCompleted = Nothing
-    , taskExec = exec
-    , taskDelay = Nothing
-    }
+  let parentAction childId = case mParent of
+        Just parent -> insert $ TaskDependency
+          { taskDependencyParent = parent
+          , taskDependencyChild  = childId
+          }
+        Nothing -> return undefined
+  taskId <- runDB $ do
+    tid <- insert $ Task
+      { taskTitle = title
+      , taskCompletion = goal
+      , taskCreated = t
+      , taskCompleted = Nothing
+      , taskExec = exec
+      , taskDelay = Nothing
+      }
+    _ <- parentAction tid
+    return tid
   selectRep $ do
     provideRep $ return $ object
       [ "taskId" .= taskId ]
