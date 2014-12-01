5eaa388a067f9c0b867fa045e8cdf58f624b63a1
Matthew Maurer <maurer@matthewmaurer.org>
2014-11-30 22:03:33 -0500

    Filter out executive tasks with unexecuted plans

--- Handler/Strike.hs
+++ Handler/Strike.hs
@@ -4,17 +4,27 @@ import Import
 import Database.Persist.Sql
 import Data.Text (pack)
 import Data.Time
+import Control.Monad
 
 taskPanel task = $(widgetFile "taskPanel")
 
+execChildren (Entity tid task) = do
+  if taskExec task
+    then do
+      rels <- selectList [TaskDependencyParent ==. tid] []
+      let childIds = map (taskDependencyChild . entityVal) rels
+      n <- count [TaskId <-. childIds, TaskCompleted ==. Nothing]
+      return $ n == 0
+    else return True
 getStrikeR :: Handler Html
 getStrikeR = do
   t <- liftIO getCurrentTime
-  tasks <- runDB $ selectList ((TaskCompleted ==. Nothing) :
-                               ([TaskDelay ==. Nothing]
-                                ||. [TaskDelay <=. Just t])
-                              )
-                              [Asc TaskCreated]
+  tasks <- runDB $ do
+    rawTasks <- selectList
+      ((TaskCompleted ==. Nothing) :
+       ([TaskDelay ==. Nothing] ||. [TaskDelay <=. Just t]))
+      [Asc TaskCreated]
+    filterM execChildren rawTasks
   defaultLayout $ do
     setTitle "Strike."
     $(widgetFile "strike")
